aaefba14d83e9a37fe80f264c347eb2df91b6fc3
Peter Martienssen <peter.martienssen@liquify-consulting.de>
2021-10-15 16:26:02 +0200

    install.sh scripts now has an usage text and command parameter as all other scripts have.

--- target/install.sh
+++ target/install.sh
@@ -1,6 +1,38 @@
 #!/bin/bash
 
-if [[ -e /tmp/Image ]]; then
-        sudo mv /tmp/Image /boot
-        sudo reboot
-fi
+usage() {
+	echo "Usage: $0 [options]"
+	echo ""
+	echo "Setup target for testing and development."
+	echo ""
+	echo "Supported options:"
+	echo "-h, --help                Show this help text."
+        echo "-k, --kernel              Setup/Reset kernel sources."
+}
+
+setup_kernel() {
+        if [[ -e /tmp/Image ]]; then
+                sudo mv /tmp/Image /boot
+                sudo reboot
+        fi
+}
+
+while [ $# != 0 ] ; do
+	option="$1"
+	shift
+
+	case "${option}" in
+	-h|--help)
+		usage
+		exit 0
+		;;
+	-k|--kernel)
+		setup_kernel
+                exit 0
+		;;
+	*)
+		echo "Unknown option ${option}"
+		exit 1
+		;;
+	esac
+done
