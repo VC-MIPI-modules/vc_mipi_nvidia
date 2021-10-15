aebcfbf45d8cb99face9d00b596aa26d0098e59d
Peter Martienssen <peter.martienssen@liquify-consulting.de>
2021-10-15 20:19:33 +0200

    config/setup.sh script now can be directly executed. Added options to define som, board and bsp.

--- bin/config/setup.sh
+++ bin/config/setup.sh
@@ -6,40 +6,53 @@ usage() {
 	echo "Setup configuration files"
 	echo ""
 	echo "Supported options:"
+        echo ".b, --board               Preselect carrier board"
         echo "-d, --driver              Setup driver configuration"
         echo "-h, --help                Show this help text"
+        echo "-p, --bsp                 Preselect board support package"
+        echo "-s, --som                 Preselect system on module"
         echo "-t, --target              Setup target configuration"
 }
 
-print_title() {
-        clear
-        if [[ -n $1 && -n $2 ]]; then
-                echo "-- Step $1 of $2 ---------------------------------------------"
-        else
-                echo "------------------------------------------------------------"
-        fi
-        echo ""
-        echo "  Vision Components MIPI CSI-2 camera driver setup"
-        echo ""
+configure() {
+    . config/base.sh
 }
 
-option=
-read_option() {
-        option=0
-        while [[ ${option} -lt $1 || ${option} -gt $2 ]]; do
+print_title() {
+        if [[ -z ${selected_som} || -z ${selected_board} || -z ${selected_bsp} ]]; then
+                clear
+                if [[ -n $1 && -n $2 ]]; then
+                        echo "-- Step $1 of $2 ---------------------------------------------"
+                else
+                        echo "------------------------------------------------------------"
+                fi
+                echo ""
+                echo "  Vision Components MIPI CSI-2 camera driver setup"
+                echo ""
+        fi
+}
+
+selection=
+read_selection() {
+        selection=0
+        while [[ ${selection} -lt $1 || ${selection} -gt $2 ]]; do
                 echo -n "  Option ($1-$2): "
-                read option
+                read selection
         done
 }
 
 choose_som() {
-        echo "------------------------------------------------------------"
-        echo "  Choose your system on module"
-        echo "    1: NVIDIA Jetson Nano (production) (https://developer.nvidia.com/embedded/jetson-nano)"
-        echo "    2: NVIDIA Jetson Nano (devkit) (https://developer.nvidia.com/embedded/jetson-nano)"
-        echo "    3: NVIDIA Jetson Xavier NX (https://developer.nvidia.com/embedded/jetson-xavier-nx)"
-        read_option 1 3
-        case ${option} in
+        if [[ -n ${selected_som} ]]; then
+                selection=${selected_som}
+        else
+                echo "------------------------------------------------------------"
+                echo "  Choose your system on module"
+                echo "    1: NVIDIA Jetson Nano (production) (https://developer.nvidia.com/embedded/jetson-nano)"
+                echo "    2: NVIDIA Jetson Nano (devkit) (https://developer.nvidia.com/embedded/jetson-nano)"
+                echo "    3: NVIDIA Jetson Xavier NX (https://developer.nvidia.com/embedded/jetson-xavier-nx)"
+                read_selection 1 3
+        fi
+        case ${selection} in
         1) som=Nano ;;
         2) som=NanoDK ;;
         3) som=XavierNX ;;
@@ -47,40 +60,50 @@ choose_som() {
 }
 
 choose_board_nano() {
-        echo "------------------------------------------------------------"
-        echo "  Choose your carrier board"
-        echo "    1: NVIDIA Jetson Nano Developer Kit B01 (https://developer.nvidia.com/embedded/jetson-nano-developer-kit)"
-        echo "    2: Auvidea JNX30-LC-PD (https://auvidea.eu/product/70804)"
-        read_option 1 2
-        case ${option} in
+        if [[ -n ${selected_board} ]]; then
+                selection=${selected_board}
+        else
+                echo "------------------------------------------------------------"
+                echo "  Choose your carrier board"
+                echo "    1: NVIDIA Jetson Nano Developer Kit B01 (https://developer.nvidia.com/embedded/jetson-nano-developer-kit)"
+                echo "    2: Auvidea JNX30-LC-PD (https://auvidea.eu/product/70804)"
+                read_selection 1 2
+        fi
+        case ${selection} in
         1) board=NV_DevKit_Nano_B01 ;;
         2) board=Auvidea_JNX30 ;;
         esac
 }
 
 choose_board_xavier_nx() {
-        echo "------------------------------------------------------------"
-        echo "  Choose your carrier board"
-        echo "    1: NVIDIA Jetson Xavier NX Developer Kit (https://developer.nvidia.com/embedded/jetson-xavier-nx-devkit)"
-        read_option 1 1
-        case ${option} in
+        if [[ -n ${selected_board} ]]; then
+                selection=${selected_board}
+        else
+                echo "------------------------------------------------------------"
+                echo "  Choose your carrier board"
+                echo "    1: NVIDIA Jetson Xavier NX Developer Kit (https://developer.nvidia.com/embedded/jetson-xavier-nx-devkit)"
+                read_selection 1 1
+        fi
+        case ${selection} in
         1) board=NV_DevKit_XavierNX ;;
         esac
 }
 
 choose_bsp() {
-        echo "------------------------------------------------------------"
-        echo "  Choose your board support package"
-        echo "    1: NVIDIA L4T 32.5.0 (https://developer.nvidia.com/embedded/linux-tegra-r325)"
-        echo "    2: NVIDIA L4T 32.5.1 (https://developer.nvidia.com/embedded/linux-tegra-r3251)"
-        echo "    3: NVIDIA L4T 32.5.2 (https://developer.nvidia.com/embedded/linux-tegra-r3251)"
-        if [[ $1 == "32.6.1" ]]; then
-                echo "    4: NVIDIA L4T 32.6.1 (https://developer.nvidia.com/embedded/linux-tegra-r3261)"
-                read_option 1 4
+        if [[ -n ${selected_bsp} ]]; then
+                selection=${selected_bsp}
         else
-                read_option 1 3
+                echo "------------------------------------------------------------"
+                echo "  Choose your board support package"
+                echo "    1: NVIDIA L4T 32.5.0 (https://developer.nvidia.com/embedded/linux-tegra-r325)"
+                echo "    2: NVIDIA L4T 32.5.1 (https://developer.nvidia.com/embedded/linux-tegra-r3251)"
+                echo "    3: NVIDIA L4T 32.5.2 (https://developer.nvidia.com/embedded/linux-tegra-r3251)"
+                if [[ $1 == "4" ]]; then
+                        echo "    4: NVIDIA L4T 32.6.1 (https://developer.nvidia.com/embedded/linux-tegra-r3261)"
+                fi
+                read_selection 1 $1
         fi
-        case ${option} in
+        case ${selection} in
         1) bsp=32.5.0 ;;
         2) bsp=32.5.1 ;;
         3) bsp=32.5.2 ;;
@@ -89,22 +112,24 @@ choose_bsp() {
 }
 
 check_configuration() {
-        clear
-        if [[ -n $1 && -n $2 ]]; then
-                echo "-- Step $1 of $2 ---------------------------------------------"
-        else
+        if [[ -z ${selected_som} || -z ${selected_board} || -z ${selected_bsp} ]]; then
+                clear
+                if [[ -n $1 && -n $2 ]]; then
+                        echo "-- Step $1 of $2 ---------------------------------------------"
+                else
+                        echo "------------------------------------------------------------"
+                fi
+                echo ""
+                echo "  Your hardware setup is:"
+                echo ""
+                echo "  System on module:      ${som}"
+                echo "  Carrier board:         ${board}"
+                echo "  Board support package: ${bsp}"
+                echo ""
                 echo "------------------------------------------------------------"
+                echo "  press enter to continue or ctrl+c to abort"
+                read
         fi
-        echo ""
-        echo "  Your hardware setup is:"
-        echo ""
-        echo "  System on module:      ${som}"
-        echo "  Carrier board:         ${board}"
-        echo "  Board support package: ${bsp}"
-        echo ""
-        echo "------------------------------------------------------------"
-        echo "  press enter to continue or ctrl+c to abort"
-        read
 }
 
 write_configuration() {
@@ -121,15 +146,15 @@ setup_driver() {
         case ${som} in
         Nano)
                 choose_board_nano
-                choose_bsp "32.6.1"
+                choose_bsp 4
                 ;;
         NanoDK)
                 choose_board_nano
-                choose_bsp
+                choose_bsp 3
                 ;;
         XavierNX) 
                 choose_board_xavier_nx
-                choose_bsp
+                choose_bsp 3
                 ;;
         esac
         check_configuration $1 $2
@@ -169,14 +194,28 @@ while [ $# != 0 ] ; do
 	shift
 
 	case "${option}" in
+        -b|--board)
+                selected_board=$1
+                shift
+                ;;
 	-d|--driver)
+                configure
 		setup_driver $1 $2
 		;;
 	-h|--help)
 		usage
 		exit 0
 		;;
+        -p|--bsp)
+                selected_bsp=$1
+                shift
+                ;;
+        -s|--som)
+                selected_som=$1
+                shift
+                ;;
 	-t|--target)
+                configure
 		setup_target
 		;;
 	esac
